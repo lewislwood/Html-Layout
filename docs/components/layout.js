36156847a091c91ec9be0888dc92678deb0dbf68
Lewis Wood <lewislwood@gmail.com>
2022-06-27 17:18:51 -0500

    started layout component class

--- docs/components/layout.js
+++ docs/components/layout.js
@@ -1,6 +1,45 @@
 import {helpLog} from "./utils.js";
+import { layoutStyle } from "./styles/layoutstyle.js";
 import {regFooter} from "./footer.js";
 import {regHeader } from "./header.js";
+
+
+
+
+customElements.define('lw-layout', 
+class lwLayout extends HTMLElement {
+constructor() {
+super();
+try {
+    const shadow = this.attachShadow({mode: "open"});
+const style = document.createElement("style"); 
+style.textContent = layoutStyle ;
+shadow.appendChild(style);
+const theOptions = options();
+
+} // Try
+catch(e) {
+    hl("lw-layout Error: " + e.message);
+} // catch
+} // constructor
+
+Options(){
+var opts = { header: true, footor: true,
+logoHeader: true, logofooter: true,
+menu: true, navBar: true,
+address: true
+ }
+
+ if (this.getAttribute("header") !== null) {
+    opts.header = ((this.getAttribute("header") === "true")  ?true : false);  
+ } // if  header
+
+return opts;
+} // Options
+
+}); // lw-layout
+
+
 
 const getLayout = () => {
     const hl = helpLog;
---
+++ docs/components/styles/layoutstyle.js
@@ -0,0 +1,8 @@
+
+
+export const layoutStyle = `
+.layout {
+    color: var(__man_fg,blue);
+    background-color: var( --main_bg, yellow);
+}
+`
