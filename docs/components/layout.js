59d941bfb7e9e1ffe7a57b23c792b8a163a302e6
Lewis Wood <lewislwood@gmail.com>
2022-06-29 15:55:25 -0500

    layout completed

--- docs/components/layout.js
+++ docs/components/layout.js
@@ -4,63 +4,75 @@ import {regFooter} from "./footer.js";
 import {regHeader } from "./header.js";
 
 
-
-
+const lwLayout = () => {
+    const hl = helpLog;
+    hl("defining Layout");
+    regHeader();
+    regFooter();
 customElements.define('lw-layout', 
 class lwLayout extends HTMLElement {
 constructor() {
 super();
 try {
+    hl("Doing Layout");
+    // Children section
+    //  Do before shadow..
+const ch = document.createElement("div"); 
+
+if(this.hasChildNodes())   {
+ch.innerHTML = this.innerHTML;
+this.innerHTML = ""
+}
+
     const shadow = this.attachShadow({mode: "open"});
 const style = document.createElement("style"); 
 style.textContent = layoutStyle ;
 shadow.appendChild(style);
-const theOptions = options();
+const layout = document.createElement("div");
+layout.setAttribute("class", "lw-layout");
+ const lOptions =this.layoutOptions ();
 
+ if( lOptions.header === "true") {
+ const h = document.createElement("lw-header");
+ h.setAttribute("logo", lOptions.logoHeader);
+ h.setAttribute("menu", lOptions.menu);
+layout.appendChild(h);
+}
+//  Now add children
+ layout.appendChild(ch);
+
+// Footer section
+if( lOptions.footor === "true") {
+    const f = document.createElement("lw-footer");
+    f.setAttribute("logo", lOptions.logofooter);
+ f.setAttribute("navBar", lOptions.navBar);
+ f.setAttribute("address", lOptions.address);
+layout.appendChild(f);    
+}
+shadow.appendChild(layout);
 } // Try
 catch(e) {
     hl("lw-layout Error: " + e.message);
 } // catch
 } // constructor
+layoutOptions() {
+return{ 
+    header: this.defaultAttr("header", 'true'),
+     footor: this.defaultAttr("footer", 'true'),
+logoHeader: this.defaultAttr("logoHeader", 'true'),
+logofooter: this.defaultAttr("logoFooter", 'true'),
+menu: this.defaultAttr("menu", 'true'),
+navBar: this.defaultAttr("navBar", 'true'),
+address: this.defaultAttr("header", 'true')
+ }} // layoutOptions
+defaultAttr( attr, defValue) {
+var a = this.getAttribute( attr);
+if (a === null) return defValue; 
+a = a.toLowerCase();
+return a; 
+} // defaultAttribute
 
-Options(){
-var opts = { header: true, footor: true,
-logoHeader: true, logofooter: true,
-menu: true, navBar: true,
-address: true
- }
+});    // class llw-layout // 
+}; // lwLayout
 
- if (this.getAttribute("header") !== null) {
-    opts.header = ((this.getAttribute("header") === "true")  ?true : false);  
- } // if  header
-
-return opts;
-} // Options
-
-}); // lw-layout
-
-
-
-const getLayout = () => {
-    const hl = helpLog;
-    helpLog("Doing Layout");
-const footer = regFooter;
-hl("Registering Footer..");
-footer();
-
-const header = regHeader;
-hl( "Registering Header");
-header();
-
-
-}
-
-
-
-
-
-
-
-
-
-export default getLayout 
+export default lwLayout;
