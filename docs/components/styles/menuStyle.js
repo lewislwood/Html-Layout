564005b0c1a75e0b49204048fd931640ef513076
Lewis Wood <lewislwood@gmail.com>
2022-07-01 13:25:43 -0500

    finished menu round 1 styling

--- docs/components/styles/menuStyle.js
+++ docs/components/styles/menuStyle.js
@@ -2,7 +2,7 @@
 
 
 const menuStyles = `
-ul.navList {
+ul.menuList {
     list-style-type:none ;
     }
     .menuItem a, a:link, a:visited , a:active, a:hover {
@@ -12,17 +12,17 @@ ul.navList {
         color: var(--navBar_fg);;
     }
     
-  .menuItem:active,  .navItem:hover {
+  .menuItem:active,  .menuItem:hover {
     border: 2px solid var( --navBar_fg, white);
   }
-  .navBar {
+  .menu {
     display:flex;
     background-color: var( --navBar_bg, red);
     color: var( --navBar_fg, cyan);
     font-weight: 600;
     flex-direction: row;
-    align-items: center;
-    justify-content: center ;
+    align-items: right;
+    justify-content: right;
     }
     .menuList {
     display: flex;
@@ -30,8 +30,8 @@ ul.navList {
     }
   
     .menuItem  {
-        background-color: var( --navBar_bg);
-        color: var( --navBar_fg);
+        background-color: var( --menu_bg);
+        color: var( --menu_fg);
         padding-left: 3px;
         padding-right: 3px;;
         }
