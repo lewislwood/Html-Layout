746ca78ff916f411b5ae2a93d868a0d03fcacdb7
Lewis Wood <lewislwood@gmail.com>
2022-08-26 19:03:48 -0500

    detials preliminary

--- docs/components/colorDetails.js
+++ docs/components/colorDetails.js
@@ -13,6 +13,12 @@ export class colorDetails {
     curVar;
 hs;
 varsList = 0;
+parent;
+current;
+container;
+heading;
+hasLoaded = false;
+
 constructor() {
 try {
     this.hs = hl;
@@ -28,18 +34,37 @@ hl("colorDetails constructor error: " + e.message);
 getContainerObjects() {
 hl("Getting details container");
 const div = document.createElement("div");
-
+div.setAttribute("class", "detailsContainer");
+div.setAttribute("id","detailContainer")
+const h = document.createElement("h3");
+h.setAttribute("id", "detailHeading");
+h.textContent = "Edit the Details for Lewis ";
+div.appendChild(h);
 return div;
 }; // getContainerObjects
 // Load up the combo boxes
 LoadColorVars() {
 hl("Loading Color Details");
+// this.heading = this.containter.querySelector("#detailHeading"); // detailHeading
 };  // loadColorVars
+
+
 connected(root) {
 hl("Connected Details.");
+this.container = root.querySelector("#detailContainter");
 this.LoadColorVars();
 } // connected
 
+setHeading(text = "Edit color") {
+
+    try {
+if (this.heading !== null) {
+this.heading.textContent = text;
+};
+    } catch(e) {
+hl("colorDetals.setHeading: " + e.message)
+    }; // catch
+}; // setHeading
 
 
 }; // class colorDetails
