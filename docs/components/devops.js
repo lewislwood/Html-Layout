ff9955728b10ba4bbb8ec6d00b87b132731f8ad8
Lewis Wood <lewislwood@gmail.com>
2022-09-22 15:10:45 -0500

    Added clipboard copy log, cleaned up, and made it more secure

--- docs/components/devops.js
+++ docs/components/devops.js
@@ -1,152 +1,189 @@
 
 
 "use strict";
-// devOps namespace
-const devOps = {
-  mode:1, // Initial mode see modes for value mode selected 
-  modes : [ "disabled", "visible", "invisible"], // determines mode values and order
-  actions: { mode :    { "Toggle": true, "key": -1,statusMessage: null} , log: { "Toggle": true, "key": -1}, clear: { "Toggle": true, "key": -1, statusMessage:null  }  },
-  styleContent : `.devOpsNotVisible {  // Default styling used to create style object
-    transform: scale(0,0);
-  z-index: 0;
-  position: absolute;
-  top: -10;
-  left: -10;
+
+class devOps {
+static mode = 2;
+static modes =[ "disabled", "visible", "invisible"] // determines mode values and order 
+static #controls = {
+  root : null, // #devOpsRoot
+  "container": null,  // #devOpsContainter
+"style": null,   // style
+container : null, 
+"logSection": null,  // #devOpsLogSection
+"header": null,  // #devOpsHeading
+'hl': null,  // #devOpsLog
+}; // Controls
+static actions =  { mode :    { "Toggle": true, "key": -1,statusMessage: null} , log: { "Toggle": true, "key": -1}, clear: { "Toggle": true, "key": -1, statusMessage:null  }, share: { "Toggle": true, "key": -1, statusMessage:null  }    }
+constructor() {
+throw new error("Static class you cannot create an instance");
+}; // constructor
+static {
+  devOps.#initialize(); 
+};
+static #initialize() {
+  const dc = devOps.#controls;
+  dc.root = document.querySelector("#devOpsRoot");
+  if (dc.root === undefined) {console.log("devOps Root not there."); }  
+  else {
+    const dr = dc.root;
+    dc.style = dr.querySelector("style");;
+    if (dc.style === undefined) { console.log("Style not found.")};
+    dc.container = dr.querySelector("#devOpsContainer");
+  if (dc.container  === undefined) {console.log("containter not there."); }
+  dc.header = dr.querySelector("#devOpsHeading");
+  if (dc.header === undefined) { console.log("header not found.")};
+  dc.hl = dr.querySelector("#devOpsLog");
+  if (dc.hl === undefined) { console.log("HL not found.")};
+  dc.clearButton = dr.querySelector("#devOpsClear");
+  if (dc.clearButton === undefined) { console.log ("clear button not found.")};
+  dc.clearButton.onclick = () => { devOps.clearLog(); {;}};
+  window.addEventListener( "keyup",devOps.keyHandler);
+  
+  devOps.loaded = true;
+  devOps.log("DevOps Loaded successfully.");
+  }; // devOps Root is defined.
+  devOps.toggleMode(devOps.mode, true);
+}; // Initialize()}
+  
+  static log(message) {
+    console.log("HelpLog: ", message);
+  try {
+    
+    const hl = devOps.#controls.hl;
+  if (hl !== null) {
+      const cur = hl.innerHTML;
+      hl.innerHTML = message + "<br/>" +cur;
+  } // if 
+  } // try
+  catch(e) {
+       console.log("HelpLog Error: ", e.message);    
+       alert(e.message);
+      }; 
+  }; // log(message)
+  static keyHandler(e) {
+    if ( e.altKey  && e.shiftKey) {
+    switch (e.key) {
+    case "C":
+      devOps.clearLog();
+    break;
+    case "D":
+    devOps.toggleMode();
+    break;
+    case "S" :
+      devOps.shareLog();
+    break;
+    }; // switch
+  }; // if alt + shift
+  }; keyHandler
+
+  static toggleMode(setMode = null, silent = false) {
+    try {
+      devOps.mode = ((setMode  === null)? devOps.mode + 1 :  setMode);
+    if ((devOps.mode > (devOps.modes.length - 1) ) || (devOps.mode < 0)) devOps.mode = 0;
+      const mode = ((devOps.modes.length > 0) ? devOps.modes[devOps.mode]: 'empty')  
+      const act = devOps.actions.mode;
+      if (act.statusMessage === null) act.statusMessage = devOps.log;
+      const c = devOps.#controls.container;
+    switch (mode) {
+    case "empty":
+      devOps.log("Empty devOps.modes: adding mode visible");
+      devOps.modes.push("visible");
+      case "visible": 
+      act.statusMessage(`Enable Log: Visible`);
+
+      c.classList.remove("devOpsNotVisible");
+      c.setAttribute("aria-hidden", "false");
+      break;
+      case "invisible" :
+        act.statusMessage(`Log not visible or accessible without screen reader`);
+        c.classList.add("devOpsNotVisible");
+      c.setAttribute("aria-hidden", "false");
+      break;
+      case "disabled" :
+        act.statusMessage(`Log disabled for everyone. Press alt plush shift + D to bring it back.`);
+        setTimeout(() => {
+        c.classList.add("devOpsNotVisible");
+      c.setAttribute("aria-hidden", "true");
+    },350);
+      break;
+      default :
+      devOps.log(`Invalid mode, ignoring value:${mode}  #${devOps.mode}}`);
+    }; // switch
+      } catch(e) {
+    devOps.log("devOps.toggleMode error: " + e.message);
+      }; // catch
+  }; // toggleMode
+  static setAction(actionName= "mode", key = null, statusMessage= devOps.log, toggle = true) {
+    try {
+    const act = devOps.actions[actionName];
+    
+    act.toggle == toggle;
+    if (key !== null) act.key = key;
+    act.statusMessage = statusMessage;
+    
+    } catch(e) {
+    devOps.log('devOps.setAction error: '+ e.message);
+    }; //  catch
+     }; // setAction
+
+     static clearLog()  {
+      try {
+        const hl = devOps.#controls.hl;
+      if (hl !== null )   {
+        hl.innerHTML ="Log Cleared.." ;
+      };  // if hl
+    }   catch(e) {
+        console.log("devOps.clearLog error: " + e.message);
+          }; // catch
+     }; // clearLog
+     static 
+     shareLog()  {
+      try {
+       const hl = devOps.#controls.hl ;
+      if (hl !== null) {
+        const act = devOps.actions.share;
+        if (act.statusMessage === null) act.statusMessage = devOps.log;  
+        
+        const txt = hl.innerHTML.split("<br>");
+        navigator.clipboard.writeText(txt.join("\n"));
+        const sm = act.statusMessage;
+        sm("Log shared to clipboard.");
+        sm("Log shared to clipboard.");
+      
+      }
+      } catch(e) {
+      devOps.log("devOps.share error: " + e.message);
+      }; // catch
+       };  // share
+       
+    
+
+}; //  class devOps
+
+const devOpsDefaultStyleCss =`
+.devOpsNotVisible {  // Default styling used to create style object
+  transform: scale(0,0);
+z-index: 0;
+position: absolute;
+top: -10;
+left: -10;
 }
 .devOpsContainer {
-    display : flex;
-    border-width: 3px;
-    border-style: outset;
-    flex-direction: row;
+  display : flex;
+  border-width: 3px;
+  border-style: outset;
+  flex-direction: row;
 margin: 5px;
 padding: 5px;
 background-color: #000080; 
 color: yellow;
 }
 #devOpsLogSection {
-    flex-direction: column;
-    align-items: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
 }
-`,
-  loaded : false,
-  "container": null,  // #devOpsContainter
-  root : null,
-"style": null,   // style
-container : null, 
-"logSection": null,  // #devOpsLogSection
-"header": null,  // #devOpsHeading
-'hl': null,  // #devOpsLog
- "clearButton": null,    // #devOpsClear,
- log: (message) => {
-  console.log("HelpLog: ", message);
+`;
 
-  try {
-    
-  if (devOps.hl !== null) {
-      const cur = devOps.hl.innerHTML;
-      devOps.hl.innerHTML = message + "<br/>" +cur;
-  } // if 
-  } // try
-  catch(e) {
-       console.log("HelpLog Error: ", e.message);    
-       alert(e.message);
-      }
- },
- clearLog : () => {
-  try {
-  if (devOps.hl !== null )   {
-    devOps.hl.innerHTML ="Log Cleared.." ;
-  };  // if hl
-  }   catch(e) {
-    console.log("devOps.clearLog error: " + e.message);
-      }; // catch
- },
- setAction: (actionName= "mode", key = null, statusMessage= devOps.log, toggle = true) => {  // Way to turn off, reassign key where message goes
-try {
-const act = devOps.actions[actionName];
 
-act.toggle == toggle;
-if (key !== null) act.key = key;
-act.statusMessage = statusMessage;
-
-} catch(e) {
-devOps.log('devOps.setAction error: '+ e.message);
-}; //  catch
- },
- toggleMode : (m) => { 
-  try {
-  devOps.mode = ((m === undefined)? devOps.mode + 1 :  m);
-if ((devOps.mode > (devOps.modes.length - 1) ) || (devOps.mode < 0)) devOps.mode = 0;
-  const mode = ((devOps.modes.length > 0) ? devOps.modes[devOps.mode]: 'empty')  
-  devOps.log(`toggling mode:${devOps.mode}  [${mode}}]`)
-  const act = devOps.actions.mode;
-  devOps.log(`#${devOps.mode}  mode:${mode}`)
-  if (act.statusMessage === null) act.statusMessage = devOps.log;
-  // act.statusMessage(`Switch to devOps mode ${mode}.`);
-switch (mode) {
-case "empty":
-  devOps.log("Empty devOps.modes: adding mode visible");
-  devOps.modes.push("visible");
-  case "visible": 
-  devOps.container.classList.remove("devOpsNotVisible");
-  devOps.container.setAttribute("aria-hidden", "false");
-  break;
-  case "invisible" :
-    devOps.container.classList.add("devOpsNotVisible");
-  devOps.container.setAttribute("aria-hidden", "false");
-  break;
-  case "disabled" :
-    devOps.container.classList.add("devOpsNotVisible");
-  devOps.container.setAttribute("aria-hidden", "true");
-  break;
-  default :
-  devOps.log(`Invalid mode, ignoring value:${mode}  #${devOps.mode}}`);
-}; // switch
-  } catch(e) {
-devOps.log("devOps.toggleMode error: " + e.message);
-  }; // catch
- }
-}; // devOps namespace
-
-// KeyHandler outside devOps namespace in order to for security reasons
-const devOpsKeyHandler = (e) => {
-const message = `You pressed key:${e.key}  code:${e.code}.`;
-if ( e.altKey  && e.shiftKey) {
-switch (e.key) {
-case "C":
-  devOps.log("You pressed clear log");
-  devOps.clearLog();
-break;
-case "D":
-  devOps.log("You pressed toggle log.");
-devOps.toggleMode();
-break;
-}; // switch
-};
-}; // devOpsKeyHandler
-
-(function init() {
-  console.log("devOps Initializing..");
-  devOps .root = document.querySelector("#devOpsRoot");
-  if (devOps .root === undefined) {console.log("devOps Root not there."); }  
-  else {
-    const dr = devOps.root;
-    devOps.style = dr.querySelector("style");;
-    if (devOps.style === undefined) { console.log("Style not found.")};
-  devOps .container = dr.querySelector("#devOpsContainer");
-  if (devOps .container  === undefined) {console.log("containter not there."); }
-  devOps.header = dr.querySelector("#devOpsHeading");
-  if (devOps.header === undefined) { console.log("header not found.")};
-  devOps.hl = dr.querySelector("#devOpsLog");
-  if (devOps.hl === undefined) { console.log("HL not found.")};
-  devOps.clearButton = dr.querySelector("#devOpsClear");
-  if (devOps.clearButton === undefined) { console.log ("clear button not found.")};
-  devOps.clearButton.onclick = () => { devOps.clearLog(); {;}};
-  window.addEventListener( "keyup",devOpsKeyHandler );
-  
-  devOps.loaded = true;
-  devOps.log("DevOps Loaded successfully.");
-  }; // devOps Root is defined.
-  })()  // initializing...
-  
 export default devOps;
